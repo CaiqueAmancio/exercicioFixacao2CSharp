6c444ed7daa83f85fa80c91d9d5ae83aa142f999
CaiqueAmancio <caique.amancio@gmail.com>
2021-06-30 16:37:09 -0300

    Primeiro commit

---
+++ exercicioFixacao2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioFixacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome Completo: ");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            char quartos = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha): ");
+            string[] vet = Console.ReadLine().Split();
+            string ultimoNome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
